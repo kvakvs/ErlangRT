a699bf2bf7b3c5ef1e81ac214417695f80ad76e2
Dmytro Lytovchenko <dmytro.lytovchenko@gmail.com>
2017-10-21 02:06:11 +0200

    RawCons is now an enum

--- src/term/raw/rcons.rs
+++ src/term/raw/rcons.rs
@@ -1,25 +1,32 @@
 use defs::Word;
 use term::lterm::LTerm;
 
-pub struct ConsPtrMut {
-  p: *mut Word,
-}
+pub enum ConsPtrMut { Ptr(*mut Word) }
+
+
+pub enum ConsPtr { Ptr(*const Word) }
+
 
 impl ConsPtrMut {
+
+  #[inline]
   pub fn from_pointer(p: *mut Word) -> ConsPtrMut {
-    ConsPtrMut { p }
+    ConsPtrMut::Ptr(p)
   }
 
   pub unsafe fn set_hd(&self, val: LTerm) {
-    *self.p = val.raw()
+    let ConsPtrMut::Ptr(p) = *self;
+    *p = val.raw()
   }
 
   pub unsafe fn set_tl(&self, val: LTerm) {
-    *self.p.offset(1) = val.raw()
+    let ConsPtrMut::Ptr(p) = *self;
+    *p.offset(1) = val.raw()
   }
 
   pub fn make_cons(&self) -> LTerm {
-    LTerm::make_cons(self.p)
+    let ConsPtrMut::Ptr(p) = *self;
+    LTerm::make_cons(p)
   }
 
 //  /// Peek into the cons, and get head value.
@@ -34,14 +41,10 @@ impl ConsPtrMut {
 }
 
 
-pub struct ConsPtr {
-  p: *const Word,
-}
-
-
 impl ConsPtr {
+  #[inline]
   pub fn from_pointer(p: *const Word) -> ConsPtr {
-    ConsPtr { p }
+    ConsPtr::Ptr(p)
   }
 
 //  pub fn raw_pointer(&self) -> *const Word { self.p }
@@ -52,11 +55,13 @@ impl ConsPtr {
 
   /// Peek into the cons, and get head value.
   pub unsafe fn hd(&self) -> LTerm {
-    LTerm::from_raw(*self.p)
+    let ConsPtr::Ptr(p) = *self;
+    LTerm::from_raw(*p)
   }
 
   /// Peek into the cons, and get tail value.
   pub unsafe fn tl(&self) -> LTerm {
-    LTerm::from_raw(*(self.p.offset(1)))
+    let ConsPtr::Ptr(p) = *self;
+    LTerm::from_raw(*p.offset(1))
   }
 }
