69dde681024d6430e560554ee4f9c17058db7bf9
Dmytro Lytovchenko <dmytro.lytovchenko@gmail.com>
2017-11-19 18:22:38 +0100

    Bif =:= and values for tests are more diverse to help tracking the crash

--- priv/test2.erl
+++ priv/test2.erl
@@ -2,8 +2,11 @@
 -export([test/0]).
 
 test() ->
-    [5,4,3,2,1] = rev([1,2,3,4,5]),
+    %% Test 1: reverse a list
+    [5, 4, 3, 2, a] = rev([a, 2, 3, 4, 5]),
+    %% Test 2: simple recursion
     0 = recurse(10),
+    %% Test 3: Reverse and equality check
     false = test_eq(),
     test_list_ops(),
     test_extcalls(),
@@ -31,8 +34,8 @@ test_try_catch() ->
 
 test_try_of_catch() ->
     try self() of
-      X when is_pid(X) -> erlang:error(hello);
-      Y -> not_ok
+        X when is_pid(X) -> erlang:error(hello);
+        Y -> not_ok
     catch error:E -> E = hello
     end.
 
@@ -44,16 +47,16 @@ test_send_receive() ->
     end.
 
 test_lc() ->
-  NumNodes = 5,
-  [ID || ID <- lists:seq(1, NumNodes)].
+    NumNodes = 5,
+    [ID || ID <- lists:seq(1, NumNodes)].
 
 test_ring() ->
     ring:create(5).
 
 test_mochijson() ->
-    mochijson:encode({struct, [ {test_neg, -10000}, {test, 10000} ]}).
-    % we need to go deeper to debuf
-    %mochijson:json_encode_proplist([{hello, "world"}], {encoder, unicode, null}).
+    mochijson:encode({struct, [{test_neg, -10000}, {test, 10000}]}).
+% we need to go deeper to debuf
+%mochijson:json_encode_proplist([{hello, "world"}], {encoder, unicode, null}).
 
 test_hof_nested() ->
     C = fun(X, _) -> X end,
@@ -62,84 +65,84 @@ test_hof_nested() ->
     lists:reverse(Z).
 
 test_hof() ->
-    F = fun(A,B) -> A =< B end,
-    [1,2,3,4] = lists:sort(F, [3,2,4,1]).
+    F = fun(A, B) -> A =< B end,
+    [10, 20, 30, 40] = lists:sort(F, [30, 20, 40, 10]),
+    [g1, g2, g3, g4] = lists:sort(F, [g3, g2, g4, g1]).
 
 test_hof_fold() ->
     % test fold
     M = 2,
     G = fun(X, A) -> (X + A) * M end,
-    15 = my_foldl(G, 0, [1,2,3,4,5]). % sum fold
+    15 = my_foldl(G, 0, [1, 2, 3, 4, 5]). % sum fold
 
-my_foldl(F, Accu, [Hd|Tail]) ->
+my_foldl(F, Accu, [Hd | Tail]) ->
     my_foldl(F, F(Hd, Accu), Tail);
 my_foldl(F, Accu, []) when is_function(F, 2) -> Accu.
 
 test_case() ->
-    [1,2,3,4] = lists:sort([3,2,4,1]).
+    [f1, f2, f3, f4] = lists:sort([f3, f2, f4, f1]).
 
 test_extcalls() ->
-    [1,2,3] = lists:reverse([3,2,1]).
+    [e1, e2, e3] = lists:reverse([e3, e2, e1]).
 
 test_eq() ->
-    [1,2,3,4] == rev([1,2,3,4]).
+    [d1, d2, d3, d4] == rev([d1, d2, d3, d4]).
 
 test_list_ops() ->
-    X = [1,2,3,4,5],
-    5 = my_last2(X),
-    4 = my_but_last2(X),
-    2 = element_at(2, X),
-    5 = len(X),
-    [5,4,3,2,1] = rev(X),
+    b5a = my_last2([b1a, b2a, b3a, b4a, b5a]),
+    b4b = my_but_last2([b1b, b2b, b3b, b4b, b5b]),
+    b2c = element_at(2, [b1c, b2c, b3c, b4c, b5c]),
+    5 = len([b1, b2, b3, b4, b5]),
+    [b5d, b4d, b3d, b2d, b1d] = rev([b1d, b2d, b3d, b4d, b5d]),
     false = is_palindrome([]),
-    false = is_palindrome(X),
-    true = is_palindrome([1,2,3,2,1]).
+    false = is_palindrome([b1e, b2e, b3e, b4e, b5e]),
+    true = is_palindrome([c1, c2, c3, c2, c1]).
 
 %f_test() ->
 %    F = fun(X) -> X * 2 end,
 %    F(2).
 
-recurse(X) when X > 0 -> recurse(X-1);
+recurse(X) when X > 0 -> recurse(X - 1);
 recurse(X) -> X.
 
 %% From 99 problems: P01 Find the last box of a list.
 %% Variant without using list reverse
 my_last2([]) -> false;
-my_last2([H|[]]) -> H;
-my_last2([_H|T]) when length(T) == 1 ->
-  [H1|[]] = T,
-  H1;
-my_last2([_H|T]) ->
-  my_last2(T).
+my_last2([H | []]) -> H;
+my_last2([_H | T]) when length(T) == 1 ->
+    [H1 | []] = T,
+    H1;
+my_last2([_H | T]) ->
+    my_last2(T).
 
 %% From 99 problems: P02 Find the last but one box of a list.
 %% Variant without using list reverse 
-my_but_last2([])-> false;
-my_but_last2([_H|[]]) -> false;
-my_but_last2([H|T]) when length(T) == 1 -> H;
-my_but_last2([_H|T]) -> my_but_last2(T).
+my_but_last2([]) -> false;
+my_but_last2([_H | []]) -> false;
+my_but_last2([H | T]) when length(T) == 1 -> H;
+my_but_last2([_H | T]) -> my_but_last2(T).
 
 %% From 99 problems: P03 Find the K'th element of a list.
 %% Find the K'th element of a list (1-based)
-element_at(K,L) when length(L) < K -> false;
-element_at(K,L)-> element_at(K,L,1).
-element_at(K,[H|_T],C) when C == K-> H;
-element_at(K,[_H|T],C) -> element_at(K,T,C+1).
+element_at(K, L) when length(L) < K -> false;
+element_at(K, L) -> element_at(K, L, 1).
+element_at(K, [H | _T], C) when C == K -> H;
+element_at(K, [_H | T], C) -> element_at(K, T, C + 1).
 
 %% From 99 problems: P04 Find the number of elements of a list.
-len([])-> 0;
-len(L) -> len(L,0).
+len([]) -> 0;
+len(L) -> len(L, 0).
 
-len([],Count) -> Count;
-len([_H|T],Count)-> len(T,Count+1).
+len([], Count) -> Count;
+len([_H | T], Count) -> len(T, Count + 1).
 
 %% From 99 problems: P05 Reverse a list.
-rev([])-> [];
-rev(L) -> rev(L,[]).
-rev([],R)-> R;
-rev([H|T],R)-> rev(T,[H|R]).
+rev([]) -> [];
+rev(L) -> rev(L, []).
+rev([], R) -> R;
+rev([H | T], R) -> rev(T, [H | R]).
 
 %% P06 Find out whether a list is a palindrome.
-is_palindrome([])-> false;
+is_palindrome([]) -> false;
 is_palindrome(L) when length(L) == 1 -> false;
 is_palindrome(L) -> case L == rev(L) of true -> true; false -> false end.
--- src/bif/bif_compare.rs
+++ src/bif/bif_compare.rs
@@ -14,10 +14,24 @@ fn module() -> &'static str { "bif_compare: " }
 pub fn ubif_seqeq_2_2(_cur_proc: &mut Process,
                       args: &[LTerm]) -> BifResult {
   assert_eq!(args.len(), 2, "{}ubif_seqeq_2_2 takes 2 args", module());
+  shared_eq(args, false)
+}
+
+
+/// Compare 2 terms with '=:='
+pub fn ubif_seq_2_2(_cur_proc: &mut Process,
+                    args: &[LTerm]) -> BifResult {
+  assert_eq!(args.len(), 2, "{}ubif_seq_2_2 takes 2 args", module());
+  shared_eq(args, true)
+}
+
+
+#[inline]
+fn shared_eq(args: &[LTerm], exact: bool) -> BifResult {
   let a: LTerm = args[0];
   let b: LTerm = args[1];
 
-  match cmp_terms(a, b, false) {
+  match cmp_terms(a, b, exact) {
     Ordering::Equal => BifResult::Value(gen_atoms::TRUE),
     _ => BifResult::Value(gen_atoms::FALSE),
   }
